2b058782ef752d4a4cef3e1e79702ac2ea6832ab
vincentbaylon <vincentbaylon@gmail.com>
2021-07-11 17:46:51 -0700

    commit tree c36f0edb225633d5ef6fee3bf225b12eaae84b14

--- index.js
+++ index.js
@@ -1,1 +1,84 @@
 // Your code here
+function createEmployeeRecord(arr) {
+    let employeeRecordObject = { 
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+
+    return employeeRecordObject
+}
+
+function createEmployeeRecords(arr) {
+    let employeeRecords = []
+
+    arr.forEach(eachArr => {
+        let newEmployeeObject = createEmployeeRecord(eachArr)
+        employeeRecords.push(newEmployeeObject)
+    })
+
+    return employeeRecords
+}
+
+function createTimeInEvent(rec, date) {
+    let newDate = date.split(' ')
+
+    let newObject = {
+        type: 'TimeIn',
+        hour: parseInt(newDate[1]),
+        date: newDate[0]
+    }
+    rec.timeInEvents.push(newObject)
+
+    return rec
+}
+
+function createTimeOutEvent(rec, date) {
+    let newDate = date.split(' ')
+
+    let newObject = {
+        type: 'TimeOut',
+        hour: parseInt(newDate[1]),
+        date: newDate[0]
+    }
+    rec.timeOutEvents.push(newObject)
+
+    return rec
+}
+
+function hoursWorkedOnDate(rec, date) {
+    let inEvent = rec.timeInEvents.find(eachDate => eachDate.date === date)
+    let outEvent = rec.timeOutEvents.find(eachDate => eachDate.date === date)
+
+    if (inEvent.date === outEvent.date) {
+        return ((outEvent.hour - inEvent.hour) / 100)
+    }
+}
+
+function wagesEarnedOnDate(rec, date) {
+    return (hoursWorkedOnDate(rec, date) * rec.payPerHour)
+}
+
+function allWagesFor(rec) {
+    let wages = 0;
+    rec.timeInEvents.forEach(eachEvent => {
+        wages = wages + wagesEarnedOnDate(rec, eachEvent.date)
+    })
+    return wages
+}
+
+function calculatePayroll(rec) {
+    let wages = 0;
+    rec.forEach(eachRec => eachRec.timeInEvents.forEach(eachEvent => {
+        wages = wages + wagesEarnedOnDate(eachRec, eachEvent.date)
+        })
+    )
+    return wages
+}
+
+function findEmployeeByFirstName(arr, name) {
+    return arr.find(eachRec => eachRec.firstName === name)
+}
